a46c342eb44aaa8049accb1a6c826bf51e5aa381
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-12-14 00:50:44 +0530

    Initial commit

--- cmd/add.go
+++ cmd/add.go
@@ -1,27 +1,28 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
+var todo string
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "add [todo]",
+	Short: "Add a new task",
+	Long: `Add a new task to the list of tasks. For example:
+	todo add "Buy milk"
+	`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		title := strings.Join(args, " ")
+		// todoManager(title)
+		fmt.Printf("Task '%s' added successfully!\n", title)
 	},
 }
 
